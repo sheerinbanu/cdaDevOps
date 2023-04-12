303073a106e55cf63abe6d645d7cae9fee77c8ce
Sheerin Banu <sheemuzz@outlook.com>
2023-04-12 13:36:14 +0200

    dev

--- routes/homepageRoutes.js
+++ routes/homepageRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
-import { getHomepage } from "../controllers/homepageController";
+import { getHomepage } from "../controllers/homepageController.js";
 
 const router = express.Router();
-router.get('/', getHomepage); 
+router.get("/", getHomepage);
 
 export default router;
