6ab937292a94a9d4493a01994209dd7f830952e4
Sheerin Banu <sheemuzz@outlook.com>
2023-04-12 14:32:49 +0200

    mongoose

---
+++ config/connectDB.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      " mongodb+srv://cdabrief@cluster0.aq1vfkm.mongodb.net/brief"
+    );
+
+    console.log("DB connected");
+  } catch (err) {
+    console.log("Error to connect with db");
+  }
+};
+export default connectDB;
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import path from "path";
 import homepageRouter from "./routes/homepageRoutes.js";
+import connectDB from "./config/connectDB.js";
 
 const __dirname = path.resolve();
 
 // Create express App
 const app = express();
+app.use(connectDB);
 
 // Set views engine
 app.set("view engine", "ejs");
