7acec944892033702e32ece046a3da265e86ccb7
Sheerin Banu <sheemuzz@outlook.com>
2023-04-12 10:58:49 +0200

    add template engine

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 import express from "express";
+import path from "path";
+
+const __dirname = path.resolve();
+
+// Create express App
 const app = express();
+
+// Set views engine
+app.set("view engine", "ejs");
+
+// Set views directory
+app.set("views", "views");
+
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
